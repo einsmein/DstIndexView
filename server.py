11403c7ca0d0c99c212bf8dd851b9354af80136c
einsmein <einsmein@hotmail.com>
2018-03-05 00:13:30 +0900

    Changed to graph.js, added daily avaragewq

--- server.py
+++ server.py
@@ -40,14 +40,11 @@ def get_real_data_mock():
 	)
 	return response
 
-@app.route('/realdata/<year_month>')
+@app.route('/hourly/<year_month>')
 def get_real_data(year_month):
 	all_dst_index = []
 	yyyy = int(year_month[:4])
 	mm = int(year_month[4:])
-	print(yyyy, mm)
-	# yyyy = int(yearmonth[:4])
-	# mm = int(yearmonth[4:])
 	current_yyyy = datetime.now().year
 	current_mm = datetime.now().month - 1
 
@@ -65,24 +62,34 @@ def get_real_data(year_month):
 		else:
 			ym += 1
 
+	response = app.response_class(
+		response = json.dumps(all_dst_index),
+		status = 200,
+		mimetype = 'application/json'
+	)
+	return response
 
-	# while (yyyy <= current_yyyy):
-	# 	while (mm < current_mm):
-	# 		if mm < 10:
-	# 			month = '0{}'.format(mm)
-	# 		else:
-	# 			month = str(mm)
-	# 		url = "http://wdc.kugi.kyoto-u.ac.jp/dst_realtime/{yy0}{yy1}{mm}/dst{yy1}{mm}.for.request".format(yy0=str(yyyy)[:2],yy1=str(yyyy)[2:4],mm=month)
-	# 		print("url: " + url)
-	# 		r = requests.get(url, allow_redirects=True)
-	# 		all_dst_index.extend(format_month_dst(r.content))
-	# 		if mm == 12:
-	# 			mm = 1
-	# 			break
-	# 		else:
-	# 			mm += 1
-	# 	yyyy += 1
+@app.route('/average/<year_month>')
+def get_avg_data(year_month):
+	all_dst_index = []
+	yyyy = int(year_month[:4])
+	mm = int(year_month[4:])
+	current_yyyy = datetime.now().year
+	current_mm = datetime.now().month - 1
 
+	ym = yyyy*100 + mm
+	current_ym = current_yyyy*100 + current_mm
+
+	while (ym <= current_ym):
+		url = "http://wdc.kugi.kyoto-u.ac.jp/dst_realtime/{yy0}{yy1}{mm}/dst{yy1}{mm}.for.request".format(yy0=str(ym)[:2],yy1=str(ym)[2:4],mm=str(ym)[4:6])
+		print("url: " + url)
+		r = requests.get(url, allow_redirects=True)
+		all_dst_index.extend(format_month_dst(r.content, True))
+
+		if ym % 100 == 12:
+			ym += 100 - 11
+		else:
+			ym += 1
 
 	response = app.response_class(
 		response = json.dumps(all_dst_index),
@@ -91,7 +98,7 @@ def get_real_data(year_month):
 	)
 	return response
 
-def format_month_dst(month_dst_raw):
+def format_month_dst(month_dst_raw, get_average=False):
 	day_dst_raw = month_dst_raw.split('\n')[:-3]
 	# ['DST1802*01RRX020   0   5   3   ...', '...' ]
 	day_dst_split = map(lambda row: 
@@ -104,19 +111,26 @@ def format_month_dst(month_dst_raw):
 		 day_dst_raw)
 	# [['DST1802*01RRX020', '0', '5', '3', ...], [...]]
 	hour_dst = []
-	map(lambda row: hour_dst.extend(map_hour_dst(row)), day_dst_split)
+	map(lambda row: hour_dst.extend(map_hour_dst(row, get_average)), day_dst_split)
 	# ['2018/02/01 1', 3]
 	return hour_dst
 
 # row is expected in the format: 
 # ['DST1802*01RRX020', '0', '5', '3', ...]
-def map_hour_dst(row):
-	hour_dst = []
-	for i in range(24):
-		date = "{yy0}{yy1}/{mm}/{dd} {h}".format(yy0=row[0][14:16], yy1=row[0][3:5], mm=row[0][5:7], dd=row[0][8:10], h=i)
-		dst = int(row[i+2])
-		hour_dst.append([date, dst])
-	return hour_dst
+def map_hour_dst(row, get_average=False):
+	all_dst = []
+	if not get_average:
+		for i in range(24):
+			date = "{yy0}{yy1}/{mm}/{dd} {h}".format(yy0=row[0][14:16], yy1=row[0][3:5], mm=row[0][5:7], dd=row[0][8:10], h=i)
+			dst = int(row[i+1])
+			all_dst.append([date, dst])
+		return all_dst
+	else:
+		date = "{yy0}{yy1}/{mm}/{dd}".format(yy0=row[0][14:16], yy1=row[0][3:5], mm=row[0][5:7], dd=row[0][8:10])
+		dst = int(row[24])
+		all_dst.append([date, dst])
+		return all_dst
+
 
 if __name__ == "__main__":
 	app.run()
