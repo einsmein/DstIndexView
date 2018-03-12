48f7fed1549871c49726cfbe0abdd016fd75cd7e
einsmein <einsmein@hotmail.com>
2018-03-12 23:41:16 +0900

    removed comments

--- server.py
+++ server.py
@@ -13,22 +13,8 @@ def index():
 def demo():
 	yearmonth = request.args.get('yearmonth')
 	print(yearmonth)
-	return render_template("demo.html", year_month = yearmonth)#"{yyyy}{mm}".format(yyyy=year,mm=month), start_date="{yyyy}-{mm}-01".format(yyyy=year,mm=month))
+	return render_template("demo.html", year_month = yearmonth)
 
-# @app.route("/realdata")
-# def get_real_data_mock():
-# 	yearmonth = '1802'
-# 	url = "http://wdc.kugi.kyoto-u.ac.jp/dst_realtime/20{yymm}/dst{yymm}.for.request".format(yymm=yearmonth)
-# 	r = requests.get(url, allow_redirects=True)
-# 	hour_dst = format_month_dst(r.content)
-# 	# print(hour_dst)
-
-# 	response = app.response_class(
-# 		response = json.dumps(hour_dst),
-# 		status = 200,
-# 		mimetype = 'application/json'
-# 	)
-	# return response
 
 @app.route('/hourly/<year_month>')
 def get_real_data(year_month):
@@ -67,6 +53,6 @@ def get_avg_data(year_month):
 
 def run():
 	app.run()
-	
+
 if __name__ == "__main__":
 	run()
